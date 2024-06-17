cdb8ca5a42c7e9fd06267c2b5f88b3d3e35c271c
Андрей <User@DESKTOP-NN5U0IC>
2024-06-17 16:58:05 +0300

    event-manager V1.2

---
+++ BOTV2/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualBasic;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Requests;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+var botClient = new TelegramBotClient("7463883649:AAHyYkO3t6Z3U-mNp6lQBL82fMeYWV7s2Dg");
+
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+User me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+
+    if (update.Message is not { } message)
+        return;
+
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+    Message sentMessage = await botClient.SendTextMessageAsync(chatId: chatId, text: "Приветсвую вас, Это бот-помощник по вопросам активностей и коммуникации. Что вас интересует", cancellationToken: cancellationToken);
+
+    object value = await HandleUpdateAsync(message.Chat.Id, IReplyMarkup: GetChatMenuButtonRequest());
+    return new ReplyKeyboardMarkup()
+    } Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>
+                    { new KeyboardButton("Привет")
+                    }
+                },
+
+
+
+    };
+    Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+    }
+}
+
+static object GetChatMenuButtonRequest()
+{
+    throw new NotImplementedException();
+
+}
+
+async Task HandlePollingErrorAsync(ITelegramBotClient client, Exception exception, CancellationToken token)
+{
+    throw new NotImplementedException();
+}
